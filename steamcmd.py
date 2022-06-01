da0a255adc1c2d86ffb2f4d74d9f729e662da635
btconst <bordeitudorconstantin@gmail.com>
2022-06-01 12:48:08 +0300

    Add dedicated scripts folder and added arg support

--- steamcmd.py
+++ steamcmd.py
@@ -1,10 +1,11 @@
 import shutil
 import os
 import tempfile
+import sys
 
 main = './steamapps/workshop/content/'
 
-script_path = input("Full install script path: ")
+script_path = sys.argv[1] #input("Full install script path: ")
 if not os.path.isfile(script_path):
     if not os.path.isfile("scripts/" + script_path):
         print("Invalid script path")
@@ -19,24 +20,32 @@ if len(script_path) < 1:
     input("")
     exit(0)
 
-mods_folder = input("Folder to install mods to: ")
-
 game_id = int(open(script_path).readlines()[0])
 
 main = main + str(game_id) + '/'
+
+for f in os.listdir(main):
+    shutil.rmtree(main+f)
 
 fp = open('a.TEMP', 'w')
 fp.write('login anonymous\n')
 
 transfer_folders = []
 
+mods_folder = None
+
 script_lines = open(script_path).readlines()
 for line in script_lines[1:]:
     line = line.strip("\n").split("#")[0]
-    if(len(line) > 0 and line.isdigit()):
+    if(len(line) > 0 and line.strip(" ").isdigit()):
         fp.write('workshop_download_item %d %s\n' % (game_id, line))
     elif(len(line) > 0 and line[0] == '!'):
         transfer_folders.append(line[1:])
+    elif(len(line) > 0 and line[0] == '@'):
+        mods_folder = line[1:]
+    else:
+        print(line)
+        
 fp.write('exit\n')
 fp.close()
 
@@ -48,6 +57,13 @@ print("\n///////////")
 print("///////////\n")
 
 os.remove('a.TEMP')
+
+if(mods_folder == None):
+    print("The install path has not been specified in the script")
+    mods_folder = input("Install path: ")
+
+if(mods_folder == None):
+    exit(0)
 
 for f in fldrs:
     source_dir = os.listdir(main)[0]
